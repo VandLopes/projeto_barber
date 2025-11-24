f76dd35fc8f7d1fbd05a097b668249bcf8e9c223
VandLopes <vanderleilopes23.vl@gmail.com>
2025-11-24 20:35:46 -0300

    alterando db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "barbearia",
-  port: "3306",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "barbearia",
+  port: process.env.DB_PORT || 3306,
 });
 
 module.exports = pool;
