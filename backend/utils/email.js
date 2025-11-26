c160570b2bc50615b386f561f5eaf7c09573c8e2
VandLopes <vanderleilopes23.vl@gmail.com>
2025-11-25 23:25:02 -0300

    alterando dominio de envio

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,27 +1,19 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // SSL obrigatório no Render
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // senha de app do Google
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function enviarEmail(to, assunto, html) {
   try {
-    const info = await transporter.sendMail({
-      from: `"Barbearia" <${process.env.EMAIL_USER}>`,
+    const response = await resend.emails.send({
+      from: "onboarding@resend.dev",
       to,
       subject: assunto,
-      html,
+      html: html,
     });
 
-    console.log("📧 Email enviado:", info.messageId);
-  } catch (err) {
-    console.error("Erro ao enviar email:", err);
+    console.log("📧 Email enviado!", response);
+  } catch (error) {
+    console.error("Erro ao enviar email:", error);
   }
 }
 
